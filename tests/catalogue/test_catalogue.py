7c8e0a4d4c8dc9a2813a03ecbcdd9cfe03701755
Martin Baláž <martin.balaz@trojsten.sk>
2025-03-11 23:01:13 +0000

    Fixed tests for new protocol

--- tests/catalogue/test_catalogue.py
+++ tests/catalogue/test_catalogue.py
@@ -20,7 +20,7 @@ def ago():
 
 @pytest.fixture
 def altaz(hyg30, ago):
-    return hyg30.altaz(ago, Time(datetime.datetime(2024, 9, 25, 21, 56, 37, tzinfo=datetime.UTC)))
+    return hyg30.altaz(ago, Time(datetime.datetime(2024, 9, 25, 21, 56, 37, tzinfo=datetime.UTC)), masked=False)
 
 
 class TestCatalogue:
@@ -31,17 +31,17 @@ class TestCatalogue:
         assert hyg30.count == 5075
 
     def test_with_planets(self, hyg30, ago):
-        assert len(hyg30.altaz(ago)) == 5075
+        assert len(hyg30.altaz(ago, masked=False)) == 5075
 
     def test_polaris(self, hyg30, ago):
-        altaz = hyg30.altaz(ago)
+        altaz = hyg30.altaz(ago, masked=False)
         # Check that the altitude of Polaris is within 1 degree of the observer's latitude
         assert altaz[47].alt.degree == pytest.approx(ago.lat.degree, abs=1)
         # Check that the azimuth of Polaris is within 1 degree of north (on northern hemisphere)
         assert abs((altaz[47].az.degree + 180) % 360 - 180) == pytest.approx(0, abs=1)
 
     def test_sirius(self, hyg30, ago):
-        altaz = hyg30.altaz(ago, Time(datetime.datetime(2024, 9, 25, 21, 56, 37, tzinfo=datetime.UTC)))
+        altaz = hyg30.altaz(ago, Time(datetime.datetime(2024, 9, 25, 21, 56, 37, tzinfo=datetime.UTC)), masked=False)
         assert altaz[0].alt.degree == pytest.approx(-25.6, abs=0.2)
         assert altaz[0].az.degree == pytest.approx(86.6, abs=0.2)
 
@@ -50,10 +50,10 @@ class TestCatalogue:
         assert len(hyg30.planets_skycoord) == 7
 
     def test_jupiter_is_really_bright(self, hyg30, ago):
-        assert hyg30.vmag(ago)[5071] == pytest.approx(-2, abs=0.5)
+        assert hyg30.vmag(ago, masked=False)[5071] == pytest.approx(-2, abs=0.5)
 
     def test_vmag(self, hyg30, ago):
-        vmag = hyg30.vmag(ago)
+        vmag = hyg30.vmag(ago, masked=False)
         assert vmag.shape == (5075,)
         assert vmag[0] == pytest.approx(-1.45, abs=0.05)
 
